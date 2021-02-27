438bcfdb827732de2a7405adec79876108063b0c
Antony Agrios <antonyagrios@gmail.com>
2021-02-27 16:15:54 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Header from "./Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-coolGray-700 text-warmGray-50 h-full w-full flex flex-col">
+      <Header></Header>
+      <div className="container m-4">
+        <h1 className="text-3xl">This is an ethereum app</h1>
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { ReactComponent as Logo } from "./logo.svg";
+
+function Header() {
+  return (
+    <nav className="flex justify-between items-center py-4 bg-pink-400">
+      <div className="flex-shrink-0 ml-10 cursor-pointer flex flex-row justify-items-center items-center">
+        <div className="h-12 w-12">
+          <Logo></Logo>
+        </div>
+        <span className="text-xl text-warmGray-50 font-semibold">
+          Ethereum app
+        </span>
+      </div>
+      <ul className="den md:flex overflow-x-hidden font-semibold">
+        <li className="mr-6 p-1 border-b-2 border-orange-500">
+          <a className="text-warmGray-50 cursor-default" href="/">
+            Home
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
